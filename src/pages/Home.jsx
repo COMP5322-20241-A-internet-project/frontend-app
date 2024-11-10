886f152000a3ca0f88ef8cf4140c67dd060c9a0f
Phoebe Fung <23000046g@connect.polyu.hk>
2024-11-10 14:31:05 +0800

    updated home css

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,29 +15,7 @@ export default function Home() {
   const navigate = useNavigate()
   return (
     <Grid container sx={styles.container} className="homeContainer">
-      <Grid item xs={12} md={12} sx={{ display: "flex", justifyContent: "center" }}>
-        <Grid item md={8} xs={12} sx={{ display: "flex", alignItems: { xs: "center", md: "start" }, height: { xs: "200px", } }}>
-          <Box sx={{
-            width: "452px", height: "437px",
-            "&.MuiBox-root": {
-              backgroundImage: `url(/src/assets/xmasRabbit.png)`, backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center"
-            }
-          }}>
-          </Box>
-          <Box sx={{
-            width: "812px", height: "437px",
-            "&.MuiBox-root": {
-              backgroundImage: `url(/src/assets/xmasSale.png)`, backgroundSize: "contain",
-              backgroundRepeat: "no-repeat",
-              backgroundPosition: "center"
-            }
-          }}>
-          </Box>
-        </Grid>
-      </Grid>
-      <Grid item xs={12} md={12} sx={{ display: "flex", justifyContent: "center", height: "100px", marginTop: { md: "250px", xs: "20px" }, alignItems: "center" }}>
+      <Grid item xs={12} md={12} sx={{ display: "flex", justifyContent: "center", height: "100px", marginTop: { md: "10px" }, alignItems: "center" }}>
         <Grid item md={8} xs={12} sx={{ display: "flex", justifyContent: "center" }}>
           <Typography
             sx={(theme) => {
@@ -56,11 +34,24 @@ export default function Home() {
             className='alerts'>30% off | Use code: CHRIS2024</Typography>
         </Grid>
       </Grid>
-      <Grid item xs={12} md={12} sx={{ display: "flex", justifyContent: "center", marginBottom: "100px", marginTop: { xs: "60px", md: "unset" } }}>
-        <Grid item md={8} xs={12} sx={{ display: "flex", alignItems: { xs: "center", md: "start" }, height: { xs: "200px", md: "300px" }, justifyContent: "center", marginTop: "20px" }}>
+      <Grid item xs={12} md={12} sx={{ display: "flex", justifyContent: "center" }}>
+        <Grid item md={8} xs={12} sx={{ display: "flex", alignItems: { xs: "center", md: "start" }, height: { xs: "200px", md: "300px" }, justifyContent: "center", paddingTop:"20px" }}>
+          <Box sx={{
+            width: "812px", height: "100%",
+            "&.MuiBox-root": {
+              backgroundImage: `url(/src/assets/xmasSalee.png)`, backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "center"
+            }
+          }}>
+          </Box>
+        </Grid>
+      </Grid>
+      <Grid item xs={12} md={12} sx={{ display: "flex", justifyContent: "center" }}>
+        <Grid item md={8} xs={12} sx={{ display: "flex", alignItems: { xs: "center", md: "start" }, height: { xs: "250px", md: "300px" }, justifyContent: "center", marginTop: { md: "20px", xs: "30px", sm:"60px" }, marginBottom: "20px", }}>
           <Box
             sx={{
-              width: "1000px", height: "400px",
+              width: "1000px", height: "320px",
               "&.MuiBox-root": {
                 backgroundImage: `url(/src/assets/newFood.jpg)`, backgroundSize: "contain",
                 backgroundRepeat: "no-repeat",
