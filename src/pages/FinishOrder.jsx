ca0f5b04adcacdbf9c64b1fbd250bbac338f5369
Phoebe Fung <23000046g@connect.polyu.hk>
2024-11-21 00:22:32 +0800

    updated finish order

--- src/pages/FinishOrder.jsx
+++ src/pages/FinishOrder.jsx
@@ -1,5 +1,16 @@
 import { Box, Typography } from '@mui/material'
 
+const styles = {
+    container: {
+        padding: "30px",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "calc(100vh - 135px)",
+        flexDirection: "column"
+    },
+}
+
 export default function FinishOrder() {
     return (<>
         <Box sx={styles.container}>
